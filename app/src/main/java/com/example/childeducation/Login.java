137deba1a473d13fdc25257d78bf970974bdfd6e
maruf <70136133+abdullahamruf@users.noreply.github.com>
2021-08-11 22:40:28 +0600

    login

--- app/src/main/java/com/example/childeducation/Login.java
+++ app/src/main/java/com/example/childeducation/Login.java
@@ -2,7 +2,9 @@ package com.example.childeducation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,11 +13,18 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Login extends AppCompatActivity {
 
-
+TextView t1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        t1=findViewById(R.id.need);
+        t1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this,Register.class));
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/childeducation/Register.java
+++ app/src/main/java/com/example/childeducation/Register.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -19,10 +21,11 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
 
-    EditText emailid,password;
+    EditText emailid,password,username;
     Button signin;
-    TextView sign;
+    TextView already;
     FirebaseAuth authentic;
+    ProgressBar p;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,13 +33,27 @@ public class Register extends AppCompatActivity {
         authentic = FirebaseAuth.getInstance();
         emailid = findViewById(R.id.email);
         password =findViewById(R.id.pass);
+        username =findViewById(R.id.username);
         signin = findViewById(R.id.signupbutton);
+        already = findViewById(R.id.already);
+       p = findViewById(R.id.pa);
+     //  if(authentic.getCurrentUser() !=null){
+          /// startActivity(new Intent(Register.this,Login.class));
+
+      // }
+        already.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Register.this,Login.class));
+            }
+        });
         signin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
              String email = emailid.getText().toString();
              String pas=  password.getText().toString();
-             if(email.isEmpty()){
+             String user=username.getText().toString();
+             if(TextUtils.isEmpty(email)){
                  emailid.setError("Enter Email");
                  emailid.requestFocus();
              }
@@ -44,15 +61,30 @@ public class Register extends AppCompatActivity {
                  password.setError("Enter Password");
                  password.requestFocus();
                 }
+             else if (user.isEmpty()){
+                 username.setError("Enter username");
+                username.requestFocus();
+             }
+
+             else if (pas.length()<6){
+                 password.setError("pass should be more than 6");
+                 password.requestFocus();
+             }
              else if (email.isEmpty() && pas.isEmpty()){
                  Toast.makeText(Register.this,"You need to enter email & Password",Toast.LENGTH_SHORT).show();
              }
+             else if (user.isEmpty()){
+                 Toast.makeText(Register.this,"You need to fill up all blank",Toast.LENGTH_SHORT).show();
+             }
+
              else if(!(email.isEmpty() && pas.isEmpty())){
+                 p.setVisibility(View.VISIBLE);
                  authentic.createUserWithEmailAndPassword(email,pas).addOnCompleteListener(Register.this, new OnCompleteListener<AuthResult>() {
                      @Override
+
                      public void onComplete(@NonNull @org.jetbrains.annotations.NotNull Task<AuthResult> task) {
                         if(!task.isSuccessful()){
-                            Toast.makeText(Register.this,"SignUp Unsuccessful, Please tryagain",Toast.LENGTH_SHORT).show();
+                            Toast.makeText(Register.this,"SignUp Unsuccessful, Please try again",Toast.LENGTH_SHORT).show();
                         }
                         else{
                             startActivity(new Intent(Register.this,Login.class));
