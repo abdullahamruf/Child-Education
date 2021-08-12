2929539977b01bb54fa1a58c5300ea611c9e7bda
zakaria masud <71184103+EzioXiyad@users.noreply.github.com>
2021-08-12 15:58:58 +0600

    scrollcheck

--- app/src/main/java/com/example/childeducation/first25.java
+++ app/src/main/java/com/example/childeducation/first25.java
@@ -3,12 +3,17 @@ package com.example.childeducation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ScrollView;
 
 public class first25 extends AppCompatActivity {
+    ScrollView view1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first25);
+        view1=findViewById(R.id.scrollview);
+        view1.setVisibility(View.VISIBLE);
     }
 }
